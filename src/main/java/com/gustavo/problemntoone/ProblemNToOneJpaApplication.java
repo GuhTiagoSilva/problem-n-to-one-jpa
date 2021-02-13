683006f7fe54f5bbd28b6012fa26786942e453fa
Gustavo Tiago <gustavo.tiago.gts@outlook.com>
2021-02-13 11:22:09 -0500

    Initial Commit

---
+++ src/main/java/com/gustavo/problemntoone/ProblemNToOneJpaApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.problemntoone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemNToOneJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProblemNToOneJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavo/problemntoone/ProblemNToOneJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.problemntoone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemNToOneJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
