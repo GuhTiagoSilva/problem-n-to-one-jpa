ff72d18298f66bdf9e673a7ba01e34662b160dac
Gustavo Tiago <gustavo.tiago.gts@outlook.com>
2021-02-13 19:23:03 -0500

    Solving N+1 Problem

--- src/main/java/com/gustavo/problemntoone/ProductService.java
+++ src/main/java/com/gustavo/problemntoone/ProductService.java
@@ -1,5 +1,7 @@
 package com.gustavo.problemntoone;
 
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -15,12 +17,12 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public Page<ProductDTO> findAllPaged (PageRequest pageRequest){
+	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Product> page = repository.findAll(pageRequest);
+		repository.findAllProductsAndCategories(page.stream().collect(Collectors.toList()));
 		return page.map(product -> new ProductDTO(product));
 	}
-	
-	
+
 }
--- src/main/java/com/gustavo/problemntoone/repositories/ProductRepository.java
+++ src/main/java/com/gustavo/problemntoone/repositories/ProductRepository.java
@@ -1,11 +1,17 @@
 package com.gustavo.problemntoone.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.gustavo.problemntoone.entities.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+	@Query("SELECT obj FROM Product obj JOIN FETCH obj.categories WHERE obj IN :products ")
+	List<Product> findAllProductsAndCategories(List<Product> products);
 
 }
